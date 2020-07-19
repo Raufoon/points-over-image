a8b10d01124f2556684a3b4fb6c40b71baf082e4
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-07-19 19:14:58 +0200

    Hide server related data

--- src/services/network.js
+++ src/services/network.js
@@ -2,15 +2,26 @@ import axios from 'axios'
 
 export function fetchPoints() {
   return axios.get('https://my-json-server.typicode.com/raufoon/points-data/points')
-    .then(response => response.data)
+    .then(response => response.data.map(point => {
+      return {
+        x: point.x, 
+        y: point.y
+      }
+    }))
 }
 
 export function submitPoints(points) {
   return axios.post('https://my-json-server.typicode.com/raufoon/points-data/points', {
-    points
+    points: points.map((point, id) => ({
+      ...point, 
+      id
+    }))
   }, {
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
+  })
+  .then(function() {
+    console.log("Submission Successful!!")
   })
 }
