41b06b309317dac39d19aa4f66e3a95dc452607e
Minhaz Raufoon <minhaz.raufoon.1567@gmail.com>
2020-07-19 18:41:44 +0200

    Allow drag and drop of points on the canvas

--- src/App.js
+++ src/App.js
@@ -6,24 +6,52 @@ import './App.css';
 
 function App() {
   const points = usePointsData()
-  const [canvas, drawPoint] = useCanvas(points)
+  const [canvas, draggedElem, selectPoint, dragPoint, dropPoint] = useCanvas(points)
 
-  const drawPointOnCanvas = useMemo(function() {
+  const getRelativeXY = useMemo(function() {
     return function(mouse) {
       const {left, top} = canvas.getBoundingClientRect()
-      const x = parseInt(mouse.clientX - left, 10)
-      const y = parseInt(mouse.clientY - top, 10)
-      drawPoint(x, y)
+      return {
+        x: parseInt(mouse.clientX - left, 10), 
+        y: parseInt(mouse.clientY - top, 10)
+      }
     }
-  }, [canvas, drawPoint])
+  }, [canvas])
+
+  const selectPointOnCanvas = useMemo(function() {
+    return function(mouse) {
+      const {x, y} = getRelativeXY(mouse)
+      selectPoint(x, y)
+    }
+  }, [getRelativeXY, selectPoint])
+
+  const dropPointOnCanvas = useMemo(function() {
+    return function(mouse) {
+      const {x, y} = getRelativeXY(mouse)
+      dropPoint(x, y)
+    }
+  }, [getRelativeXY, dropPoint])
+
+  const dragPointOnCanvas = useMemo(function() {
+    return function(mouse) {
+      if (!draggedElem) return;
+      const {x, y} = getRelativeXY(mouse)
+      dragPoint(x, y)
+    }
+  }, [getRelativeXY, draggedElem, dragPoint])
 
   return (
     <div className="App">
       <label>Drag the points to adjust them</label>
-      
+     
       <div id="canvasContainer">
         <img id="canvasBackground" src={sampleImage} alt="canvas background"/>
-        <canvas id="canvas" onMouseUp={drawPointOnCanvas}></canvas>
+        
+        <canvas 
+          id="canvas" 
+          onMouseUp={dropPointOnCanvas} 
+          onMouseMove={dragPointOnCanvas}
+          onMouseDown={selectPointOnCanvas}></canvas>
       </div>
       
       <button>Upload New Points</button>
--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,10 +1,43 @@
-import {useEffect} from 'react'
+import {useState, useRef, useEffect, useMemo} from 'react'
 
 let canvas;
 
 export default function useCanvas(initialPoints) {
+  const pointsRef = useRef(initialPoints || [])
+  const [draggedElem, setDraggedElem] = useState(null)
+  const DOT = 4
+  const HDOT = 2
+
+  const drawPoint = useMemo(function() {
+    return function(x, y) {
+      const context = canvas.getContext('2d')
+      context.fillStyle = "#000"
+      context.fillRect(x-HDOT, y-HDOT, DOT, DOT)
+      context.beginPath()
+      context.arc(x, y, DOT + 2, 0, 2 * Math.PI)
+      context.stroke()
+    }
+  }, [])
+
+  const drawAllPoints = useMemo(function() {
+    return function() {
+      const context = canvas.getContext('2d')
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      pointsRef.current.forEach((({x, y}) => drawPoint(x, y)))
+    }
+  }, [drawPoint, pointsRef])
+
+  /*
+  const erasePoint = useMemo(function() {
+    return function(x, y) {
+      const context = canvas.getContext('2d')
+      context.fillStyle = "#000"
+      context.clearRect(x-(DOT*3), y-(DOT*3), (DOT*6), (DOT*6))
+    }
+  }, [])
+  */
   
-  useEffect(function effect() {
+  useEffect(function initCanvas() {
     canvas = document.getElementById('canvas')
     const background = document.getElementById('canvasBackground')  
     background.onload = function() {
@@ -12,18 +45,45 @@ export default function useCanvas(initialPoints) {
       canvas.height = parseInt(background.offsetHeight, 10)
     }
     if (initialPoints) {
-      initialPoints.forEach(({x, y}) => drawPoint(x, y))
+      pointsRef.current = initialPoints
+      drawAllPoints()
     }
-  }, [initialPoints])
+  }, [initialPoints, drawAllPoints])
 
-  function drawPoint(x, y) {
-    const context = canvas.getContext('2d')
-    context.fillStyle = "#000"
-    context.fillRect(x, y, 1, 1)
-    context.beginPath();
-    context.arc(x, y, 4, 0, 2 * Math.PI);
-    context.stroke();
-  }
+  const selectPoint = useMemo(function() {
+    return function(px, py) {
+      const pointsOnCanvas = pointsRef.current
+      const selectedPoints = pointsOnCanvas.filter(function(point) {
+        const {x, y} = point
+        return (Math.abs(px-x) < DOT+2) && (Math.abs(py-y) < DOT+2) 
+      })
 
-  return [canvas, drawPoint]
+      if (selectedPoints.length > 0) {
+        setDraggedElem(selectedPoints[0])
+        console.log("SELECT", selectedPoints[0].x, selectedPoints[0].y)
+      }
+    }
+  }, [pointsRef, setDraggedElem])
+
+  const dragPoint = useMemo(function() {
+    return function(x, y) {
+      if (draggedElem) {
+        draggedElem.x = x
+        draggedElem.y = y
+        drawAllPoints()
+      }
+    }
+  }, [draggedElem, drawAllPoints])
+
+  const dropPoint = useMemo(function() {
+    return function(x, y) {
+      if (draggedElem) {
+        drawAllPoints()
+        console.log("Dropped", pointsRef.current)
+      }
+      setDraggedElem(null)
+    }
+  }, [draggedElem, setDraggedElem, drawAllPoints])
+
+  return [canvas, draggedElem, selectPoint, dragPoint, dropPoint]
 }
